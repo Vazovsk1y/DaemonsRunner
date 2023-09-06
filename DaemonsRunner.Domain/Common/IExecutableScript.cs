319f4f612eca07e5d54569f3dc8b6de688d268c3
Vazovsk1y <anolrey.b@gmail.com>
2023-09-06 19:40:23 +0300

    -modified domain implementation.

---
+++ DaemonsRunner.Domain/Common/IExecutableScript.cs
@@ -0,0 +1,12 @@
+﻿using DaemonsRunner.Domain.Models;
+
+namespace DaemonsRunner.Domain.Common;
+
+public interface IExecutableScript
+{
+	ExecutableFile? ExecutableFile { get; }
+
+	string Title { get; }
+
+	string Command { get; }
+}
---
+++ DaemonsRunner.Domain/Models/Script.cs
@@ -0,0 +1,33 @@
+﻿using DaemonsRunner.Domain.Common;
+
+namespace DaemonsRunner.Domain.Models;
+
+public class Script : IExecutableScript
+{
+    public ExecutableFile? ExecutableFile { get; }
+
+    public string Title { get; }
+
+    public string Command { get; }
+
+    private Script(
+        string title,
+        string command,
+        ExecutableFile? executableFile = null)
+    {
+        ExecutableFile = executableFile;
+        Title = title;
+        Command = command;
+    }
+
+    public static Script Create(
+        string title,
+        string command,
+        ExecutableFile? executableFile = null)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(title, nameof(title));
+        ArgumentException.ThrowIfNullOrEmpty(command, nameof(command));
+
+        return new Script(title, command, executableFile);
+    }
+}
